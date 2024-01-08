ebe3c7462d2331ea612399fc9ee9ea56c7212242
Collin Caram <collin@nubrakes.com>
2024-01-08 14:58:55 -0600

    initial commit with some added logic

--- api/index.py
+++ api/index.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from llama_index.program import OpenAIPydanticProgram
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Transcript(BaseModel):
+    """The transcript of the podcast episode"""
+
+    content: str
+
+
 @app.get("/api/python")
 def hello_world():
+    program = OpenAIPydanticProgram.from_defaults(
+        output_cls=Transcript,
+        prompt_template_str="Generate a fake podcast transcript",
+        verbose=True,
+    )
+    output = program()
     return {"message": "Hello World"}
