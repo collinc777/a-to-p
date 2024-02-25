34f1d9f7912f897d18b4601f925b75748782fbc7
Collin Caram <collin@opus.so>
2024-02-25 11:28:32 -0600

    fix parent

--- app/episode/[id]/EpisodeDetails.tsx
+++ app/episode/[id]/EpisodeDetails.tsx
@@ -4,30 +4,33 @@ import Link from "next/link";
 import DownloadButton from "./DownloadButton";
 import { Button } from "@/components/ui/button";
 import { FragmentOf, ResultOf, readFragment } from "@/app/graphql";
-import { EpisodeFragment } from "@/app/queries";
+import { EpisodeFragment, ExtractedArticleFragment } from "@/app/queries";
 
 export const EpisodeDetails = ({
   episode,
 }: {
   episode: ResultOf<typeof EpisodeFragment>;
 }) => {
+  const extractedArticle = readFragment(
+    ExtractedArticleFragment,
+    episode.extractedArticle
+  );
   const isEditable = episode?.status === "done";
-  const transcript = episode?.transcript;
   return (
     <main className="px-4 space-y-2 pb-4">
       <div>
-        {episode?.extractedArticle?.url ? (
+        {extractedArticle?.url ? (
           <Link
-            href={episode?.extractedArticle?.url}
+            href={extractedArticle?.url}
             className="text-blue-500 hover:text-blue-700 underline"
           >
             <h1 className="text-3xl font-bold">
-              Generated Episode: {episode?.extractedArticle?.title}
+              Generated Episode: {extractedArticle?.title}
             </h1>
           </Link>
         ) : (
           <h1 className="text-xl">
-            Generated Episode: {episode?.extractedArticle?.title}
+            Generated Episode: {extractedArticle?.title}
           </h1>
         )}
       </div>
@@ -46,7 +49,7 @@ export const EpisodeDetails = ({
       </div>
       <div>
         <h2 className="text-2xl font-semibold">Transcript</h2>
-        {transcript ? (
+        {episode.transcript ? (
           <TranscriptViewer
             episodeId={episode.id!}
             transcriptFrag={episode?.transcript}
