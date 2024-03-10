929a189c9d309f9584071b63b608c4f26a798bc5
Collin Caram <collin@opus.so>
2024-03-10 15:51:41 -0500

    try this

--- app/episode/[id]/DownloadButton.tsx
+++ app/episode/[id]/DownloadButton.tsx
@@ -1,20 +1,11 @@
-import { Button, buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
+"use client";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function DownloadButton({ url }: { url: string }) {
-  const split = url.split("/");
-  const last = split[split.length - 1];
-  const newurl = `/api/download_mp3?fileName=${last}`;
-
   return (
-    <Link
-      prefetch={false}
-      href={newurl}
-      download={true}
-      className={buttonVariants({})}
-    >
+    <a href={url} download={"file"}>
       Download
-    </Link>
+    </a>
   );
 }
