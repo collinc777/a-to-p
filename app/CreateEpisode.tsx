57fa08b892873aeec95347851072ecd451a1231d
Collin Caram <collin@opus.so>
2024-03-13 21:06:31 -0500

    some frontend stuff for getting in the options. need to figureout the form flow

--- app/CreateEpisode.tsx
+++ app/CreateEpisode.tsx
@@ -10,17 +10,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
-const podcastFormats = [
-  { displayValue: "Monologue (Solo)", value: "monologue" },
-  { displayValue: "Co-hosted (Dialogue)", value: "dialogue" },
-  { displayValue: "Interview", value: "interview" },
-  { displayValue: "Panel", value: "panel" },
-  { displayValue: "Educational", value: "Educational" },
-  { displayValue: "Other", value: "other" },
-];
+import { graphql } from "./graphql";
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+import { usePostHog } from "posthog-js/react";
 
 export function CreateEpisode() {
+  const { data } = useSuspenseQuery(EpisodeFormatChoicesQuery);
+  const ph = usePostHog();
   return (
     <main className="flex-1 py-8 px-4">
       <div className="container mx-auto">
@@ -37,22 +33,48 @@ export function CreateEpisode() {
             placeholder="Paste your text or URL here"
             type="text"
           />
-          <Select>
+          <Select
+            name="podcastFormatSelector"
+            onValueChange={(e) => {
+              ph.capture("Podcast Format Selected", {
+                podcastFormat: e,
+              });
+              const selectedFormat = data.episodeFormatChoices.find(
+                (format) => format.value === e
+              );
+              if (!selectedFormat?.isReady) {
+              }
+            }}
+          >
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Podcast Format..." />
             </SelectTrigger>
             <SelectContent>
-              {podcastFormats.map((format) => (
-                <SelectItem key={format.value} value={format.value}>
-                  {format.displayValue}
+              {data.episodeFormatChoices.map((format) => (
+                <SelectItem
+                  key={format.value}
+                  value={format.value}
+                  disabled={!format.isReady}
+                >
+                  {format.displayName}
                 </SelectItem>
               ))}
+              <SelectItem value="other">Other</SelectItem>
             </SelectContent>
           </Select>
-
           <FormButton />
         </form>
       </div>
     </main>
   );
 }
+
+const EpisodeFormatChoicesQuery = graphql(`
+  query EpisodeFormatChoicesQuery {
+    episodeFormatChoices {
+      value
+      displayName
+      isReady
+    }
+  }
+`);
