f71d125e8361001844bf6d22ec9843a0c7dc9d50
Collin Caram <collin@opus.so>
2024-03-27 18:50:27 -0500

    fix env

--- api/settings.py
+++ api/settings.py
@@ -1,25 +1,19 @@
 from functools import lru_cache
-from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
     openai_api_key: str
-    aws_access_key_id: Optional[str] = None
-    aws_secret_access_key: Optional[str] = None
-    bucket_access_key_id: Optional[str] = None
-    bucket_secret_access_key: Optional[str] = None
-    bucket_url: Optional[str] = None
-    bucket_public_url: Optional[str] = None
-    aws_region: Optional[str] = None
-    aws_default_region: Optional[str] = None
-    replicate_api_token: Optional[str] = None
+    bucket_access_key_id: str
+    bucket_secret_access_key: str
+    bucket_url: str
+    bucket_public_url: str
+    aws_region: str
+    aws_default_region: str
     database_url: str
-    sentry_dsn: Optional[str] = None
-    playht_user_id: Optional[str] = None
-    playht_secret_key: Optional[str] = None
-    stage: Optional[str] = None
+    sentry_dsn: str
+    stage: str
     edenai_api_key: str
 
 
